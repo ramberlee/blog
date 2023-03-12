76b5cc3c08c1d3d37341bdad44157992f7d311b6
ramber <lirobinho@sina.cn>
2023-03-12 14:33:51 +0800

    初始化提交

---
+++ content/meta/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: "Ramber", // <title>
+  shortSiteTitle: "Ramber", // <title> ending for posts and pages
+  siteDescription: "Ramber",
+  siteUrl: "https://www.ramber.top",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "zh",
+
+  /* author */
+  authorName: "Ramber",
+  authorTwitterAccount: "",
+  authorGithub: "https://github.com/ramberlee",
+  // authorStackoverflow: "https://stackexchange.com/users/5678032/atte-juvonen?tab=accounts",
+  // authorLinkedin: "https://linkedin.com/in/atte-juvonen-86b886113",
+  // authorCodeforces: "https://codeforces.com/profile/baobab",
+  // authorYoutube: "https://www.youtube.com/channel/UCfHqfHoSYL0V_fXA_JQkeLw?view_as=subscriber",
+  // authorUnsplash: "https://unsplash.com/@baobabkoodaa",
+  // authorYCombinator: "https://news.ycombinator.com/user?id=baobabKoodaa",
+
+  /* header text */
+  headerTitle: "Ramber",
+  headerSubTitle: "i blog & i code",
+
+  /* manifest.json */
+  manifestName: "ramber",
+  manifestShortName: "ramber", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+};
---
+++ content/posts/2021-04-01--website-moves-your-cursor/index.md
@@ -0,0 +1,22 @@
+---
+title: I made a website that moves your mouse cursor
+tags: ["web", "everything-is-broken", "i-made-thing"]
+cover: mouses.jpg
+author: ramber
+---
+
+<re-img
+    src="mouses.jpg"
+    title="Photo by Antonio Manaligod on Unsplash"
+    href="https://unsplash.com/photos/T_2UFB_xwzw"
+    >
+</re-img>
+
+A long-running question on <a href="https://stackoverflow.com/questions/4752501/move-the-mouse-pointer-to-a-specific-position" target="_blank">StackOverflow</a>
+asks: can a website move your mouse cursor? The answer which is marked as ''accepted'' unequivocally states ''it isn't possible''. The most up-voted answer (with 195 upvotes) is even more dramatic:
+
+> You cannot move the mousepointer with javascript. Just think about the implications for a second.
+
+Well, I like a good challenge. And guess what? <a href="/mouse/" target="_blank">It's done. Click here to try it for yourself</a>.
+
+_Edit: I originally released this as a fun little April's Fools project. After the mouse moving demo turned out to be popular, I decided to create a mini-game around the mechanic: <a href="https://ouija.ramber.top" target="_blank">Ouija Online, a web horror experience</a>._
